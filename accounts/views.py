48525ce044a089dd647020f3e14ac07145d6a1cb
AliJalili <pandaj200021@gmail.com>
2025-07-20 19:34:23 +0330

    UserLogoutView added

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,10 @@ from .models import User
 from .serializers import UserRegisterSerializer, AdminUserSerializer
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class UserRegisterView(CreateAPIView):
@@ -18,3 +22,21 @@ class AdminUserViewSet(ModelViewSet):
         instance.is_active = False
         instance.save()
 
+
+class UserLogoutView(APIView):
+
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+
+            if token.payload['user_id'] != request.user.id:
+                return Response(data={'detail':'Token does not belongs to authentication user'},
+                                status=status.HTTP_403_FORBIDDEN)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        
+        except TokenError as err:
+            return Response(data={'detail':str(err)}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as err:
+            return Response(data={'detail':str(err)}, status=status.HTTP_400_BAD_REQUEST)
