37dc5c8a5beeed571cec003ee9f795e81b7b5684
AliJalili <pandaj200021@gmail.com>
2025-07-20 17:47:16 +0330

    UserRegisterView and AdminUserViewSet added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from .models import User
+from .serializers import UserRegisterSerializer, AdminUserSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class UserRegisterView(CreateAPIView):
+    serializer_class = UserRegisterSerializer
+
+
+class AdminUserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated,]
+    serializer_class = AdminUserSerializer
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
+
