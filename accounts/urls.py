ad653241a0805019ed40c0200a60375ebcb3f683
AliJalili <pandaj200021@gmail.com>
2025-07-22 21:28:56 +0330

    Add send-otp and token-by-otp endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,6 +12,8 @@ app_name = 'accounts'
 urlpatterns =[
     path('register/', views.UserRegisterView.as_view(), name='user_register'),
     path('logout/', views.UserLogoutView.as_view(), name='user_logout'),
+    path('auth/send-otp/', views.SendOTPView.as_view(), name='send_otp_code'),
+    path('auth/token-by-otp/', views.OTPLoginView.as_view(), name='token_by_otp'),
 ] 
 
 urlpatterns += router.urls
