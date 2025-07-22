dd8f5b93eb8902505c84591bdad81203bc7e7e65
AliJalili <pandaj200021@gmail.com>
2025-07-22 21:24:23 +0330

    Register OTPCode and UserProfile models in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+from .models import User, OTPCode, UserProfile
 from django.contrib.auth.models import Group
+
 
 class UserAdmin(BaseUserAdmin):
     model = User
@@ -20,6 +21,21 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {'fields':('phone', 'email', 'username', 'password', 'is_admin', 'is_active')}),
         )
-        
+    
+
+class OTPCodeAdmin(admin.ModelAdmin):
+    ordering = ('-created_at',)
+    list_display = ('phone', 'code', 'created_at', 'is_expired',)
+    search_fields = ('phone',)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    ordering = ('-created_at',)
+    list_display = ('user', 'name', 'surname',)
+    search_fields = ('name', 'surname')
+
+
 admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
+admin.site.register(OTPCode, OTPCodeAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
