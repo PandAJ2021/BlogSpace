a7e406d735e644c7beb97fe2ed19e8164c96874e
AliJalili <pandaj200021@gmail.com>
2025-07-23 20:56:46 +0330

    Add blog endpoints to main URL configuration

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
