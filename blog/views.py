12f14559e3dd85a6feebd80147d8116ff8d5dc56
AliJalili <pandaj200021@gmail.com>
2025-07-23 20:51:17 +0330

    ListRetrievePostView and UserPostView added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from .models import Post, Comment, Category, Tag
+from .serializers import PublicPostSerializer, UserPostSerilaizer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class ListRetrievePostView(ListAPIView, RetrieveAPIView, GenericViewSet):
+    serializer_class = PublicPostSerializer
+    lookup_field = 'slug'
+
+    def get_queryset(self):
+        return Post.objects.filter(is_published=True)
+
+
+class UserPostView(ModelViewSet):
+    serializer_class = UserPostSerilaizer
+    permission_classes = [IsAuthenticated,]
+    lookup_field = 'slug'
+
+    def get_queryset(self):
+        return Post.objects.filter(author=self.request.user)
