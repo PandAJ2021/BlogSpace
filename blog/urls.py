0ef826894fa2e44e3509719601cb1da47788eee1
AliJalili <pandaj200021@gmail.com>
2025-07-23 20:54:10 +0330

    Add ListRetrievePostView, UserPostView endpoints

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from . import views
+
+router = SimpleRouter()
+router.register(r'posts', views.ListRetrievePostView, basename='posts')
+router.register(r'my-posts', views.UserPostView, basename='my_posts')
 
 app_name = 'blog'
 urlpatterns = []
+urlpatterns += router.urls
