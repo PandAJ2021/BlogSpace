57334ea70aea91979031b978a664665d2c429fa6
AliJalili <pandaj200021@gmail.com>
2025-07-24 19:49:26 +0330

    - Added PostLike and CommentLike models. - Added `likes_count` property to Post and Comment models. - Added `is_reply` property to Comment model. - Updated Comment model with self-referential 'parent' field.

--- blog/models.py
+++ blog/models.py
@@ -25,17 +25,30 @@ class Post(models.Model):
             unique_identifier = str(uuid.uuid4())[:8]
             self.slug = f"{base_slug}-{unique_identifier}"
         return super().save(*args, **kwargs)
+    
+    @property
+    def likes_count(self):
+        return self.likes.count()
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comments')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='comments')
     content = models.TextField()
     is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'{self.user} comment on {self.post}'
+    
+    @property
+    def is_reply(self):
+        return self.parent is not None
+    
+    @property
+    def likes_count(self):
+        return self.likes.count()
 
 
 class Category(models.Model):
@@ -59,3 +72,15 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class PostLike(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='post_ikes')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CommentLike(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comment_likes')
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='likes')
+    created_at = models.DateTimeField(auto_now_add=True)
