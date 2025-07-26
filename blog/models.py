4655306580a6ddd18b81fc3e53595ddba082364d
AliJalili <pandaj200021@gmail.com>
2025-07-26 15:53:51 +0330

    Add Meta options and __str__ methods to PostLike and CommentLike models

--- blog/models.py
+++ blog/models.py
@@ -79,8 +79,22 @@ class PostLike(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        unique_together  = ('user', 'post')
+        ordering = ('-created_at',)
+    
+    def __str__(self):
+        return f'{self.user.username} liked {self.post}'
+
 
 class CommentLike(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='comment_likes')
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='likes')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together  = ('user', 'comment')
+        ordering = ('-created_at',)
+    
+    def __str__(self):
+        return f'{self.user.username} liked {self.comment}'
