deba226bbcd31265ba271aed61db1fd8639dce07
AliJalili <pandaj200021@gmail.com>
2025-07-26 17:47:12 +0330

    Register PostLike, CommentLike, and user-specific comment views in URLConf

--- blog/urls.py
+++ blog/urls.py
@@ -3,9 +3,15 @@ from rest_framework.routers import SimpleRouter
 from . import views
 
 router = SimpleRouter()
-router.register(r'posts', views.ListRetrievePostView, basename='posts')
+router.register(r'posts', views.ReadOnlyPostView, basename='posts')
 router.register(r'my-posts', views.UserPostView, basename='my_posts')
+router.register(r'my-comments', views.UserCommentView, basename='my-comments')
 
 app_name = 'blog'
-urlpatterns = []
+urlpatterns = [
+    path('post/comments/', views.ListCommentsView.as_view(), name='post_comments'),
+    path('post/like/<slug:post_slug>/', views.PostLikeView.as_view(), name='post_like'),
+    path('post/comment/like/<int:comment_id>/', views.CommentLikeView.as_view(), name='comment_like'),
+
+]
 urlpatterns += router.urls
