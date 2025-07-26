fe358b363a6f98124dd0b934df29f5159fe6276f
AliJalili <pandaj200021@gmail.com>
2025-07-26 17:39:42 +0330

    Added PostLikeView and CommentLikeView

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
-from .models import Post, Comment, Category, Tag
-from .serializers import PublicPostSerializer, UserPostSerilaizer, CommentSerializer
+from .models import Post, Comment, PostLike, CommentLike
+from .serializers import PublicPostSerializer, UserPostSerilaizer, CommentSerializer, PostLikeSerializer, CommentLikeSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
 from django.shortcuts import get_object_or_404
 from .permissions import IsOwner
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 class ReadOnlyPostView(ReadOnlyModelViewSet):
@@ -42,3 +45,39 @@ class UserCommentView(ModelViewSet):
 
     def get_queryset(self):
         return Comment.objects.filter(user=self.request.user)
+    
+
+class PostLikeView(APIView):
+    permission_classes = [IsAuthenticated,]
+    
+    def post(self, request, post_slug):
+        post = get_object_or_404(Post, slug=post_slug)
+        like, created = PostLike.objects.get_or_create(user=request.user, post=post)
+        if not created:
+            return Response({'detail':'Already liked'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = PostLikeSerializer(instance=like)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def delete(self, request, post_slug):
+        post = get_object_or_404(Post, slug=post_slug)
+        like = get_object_or_404(PostLike, user=request.user, post=post)
+        like.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentLikeView(APIView):
+    permission_classes = [IsAuthenticated,]
+    
+    def post(self, request, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        like, created = CommentLike.objects.get_or_create(user=request.user, comment=comment)
+        if not created:
+            return Response({'detail':'Already liked'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = CommentLikeSerializer(instance=like)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def delete(self, request, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        like = get_object_or_404(CommentLike, user=request.user, comment=comment)
+        like.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
