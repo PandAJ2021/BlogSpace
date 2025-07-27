4ad3b30800de0ad15d0abe7f8a7a4b63c3438a71
AliJalili <pandaj200021@gmail.com>
2025-07-27 23:06:05 +0330

    Register follow and subscribe routes using SimpleRouter in relationships.urls

--- relationships/urls.py
+++ relationships/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
+from rest_framework.routers import SimpleRouter
+from . import views
 
+
+router = SimpleRouter()
+router.register(r'follow', views.FollowView, basename='follow')
+router.register(r'subscribe', views.SubscribeListCreateView, basename='subscribe')
+router.register(r'subscribe/update', views.SubscribeUpdateView, basename='subscribe_update')
 
 app_name = 'relationships'
 urlpatterns = []
+urlpatterns += router.urls
