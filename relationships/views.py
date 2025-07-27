0977cf02f82cfd9129a1434c71ab53e53197111f
AliJalili <pandaj200021@gmail.com>
2025-07-27 23:04:36 +0330

    Add views for subscribing: SubscribeListCreateView and SubscribeUpdateView

--- relationships/views.py
+++ relationships/views.py
@@ -1,7 +1,7 @@
-from rest_framework.generics import ListCreateAPIView, DestroyAPIView
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView, UpdateAPIView
 from rest_framework.viewsets import GenericViewSet
 from .models import Follow, Subscribe
-from .serializers import FollowSerializer
+from .serializers import FollowSerializer, SubscribeUpdateSerializer ,SubscribeCreateSerializer
 from rest_framework.permissions import IsAuthenticated
 
 
@@ -12,3 +12,21 @@ class FollowView(ListCreateAPIView, DestroyAPIView, GenericViewSet):
 
     def get_queryset(self):
         return Follow.objects.filter(follower=self.request.user)
+
+
+class SubscribeListCreateView(ListCreateAPIView, GenericViewSet):
+    serializer_class = SubscribeCreateSerializer
+    permission_classes = [IsAuthenticated,]
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return Subscribe.objects.filter(subscriber=self.request.user)
+
+
+class SubscribeUpdateView(UpdateAPIView, GenericViewSet):
+    serializer_class = SubscribeUpdateSerializer
+    permission_classes = [IsAuthenticated,]
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return Subscribe.objects.filter(subscriber=self.request.user)
