0506492410a4cb2f6a100930bfd47a914dc82529
AliJalili <pandaj200021@gmail.com>
2025-07-27 19:28:48 +0330

    FollowView added

--- relationships/views.py
+++ relationships/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, DestroyAPIView
+from rest_framework.viewsets import GenericViewSet
+from .models import Follow, Subscribe
+from .serializers import FollowSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class FollowView(ListCreateAPIView, DestroyAPIView, GenericViewSet):
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticated,]
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return Follow.objects.filter(follower=self.request.user)
