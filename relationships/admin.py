175882e6013de2056ef1b74742bbf011b0510e9f
AliJalili <pandaj200021@gmail.com>
2025-07-27 23:00:21 +0330

    Subscribe and Follow models registered in admin site

--- relationships/admin.py
+++ relationships/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Follow, Subscribe
 
-# Register your models here.
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    ordering = ('-created_at',)
+    list_display = ('author', 'follower', 'created_at',)
+    search_fields = ('author',)
+
+
+@admin.register(Subscribe)
+class SubscribeAdmin(admin.ModelAdmin):
+    ordering = ('-updated_at',)
+    list_display = ('author', 'subscriber', 'created_at', 'updated_at', 'expired_at', 'is_active',)
+    search_fields = ('author',)
+    list_filter = ('duration',)
