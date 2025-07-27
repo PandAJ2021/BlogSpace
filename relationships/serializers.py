99ad5be0e8fdfebcc6b90bb854a21f7427acdfab
AliJalili <pandaj200021@gmail.com>
2025-07-27 19:23:25 +0330

    Refactor FollowSerializer to use SlugField for author; add validation for username and self-following

--- relationships/serializers.py
+++ relationships/serializers.py
@@ -1,24 +1,38 @@
 from rest_framework import serializers
-from models import Follow, Subscribe
+from .models import Follow, Subscribe
+from accounts.models import User
 
 
 class FollowSerializer(serializers.ModelSerializer):
+    author = serializers.SlugField('username')
 
     class Meta:
         model = Follow
-        fields = ['author', 'follower', 'created_at']
+        fields = ['id', 'author' , 'created_at']
         extra_kwargs = {
             'created_at':{'read_only':True},
+            'id':{'read_only':True},
         }
             
     def validate(self, data):
-        author = data['author']
-        follower = data['follower']
+        follower = self.context['request'].user
+
+        username = data.pop('author', None)
+        if not username:
+            raise serializers.ValidationError({'author':'this field is required'})
+        
+        try:
+            author = User.objects.get(username=username)
+        except User.DoesNotExist:
+            raise serializers.ValidationError({'author':'User Not found'})
 
         if author == follower:
-            raise serializers.ValidationError('You cannot follow yourself')
+            raise serializers.ValidationError({'author':'You cannot follow yourself'})
         
         if Follow.objects.filter(author=author, follower=follower).exists():
-            raise serializers.ValidationError(f'You already follow {author}')
+            raise serializers.ValidationError({'author':f'You already follow {author}'})
+        
+        data['author'] = author
+        data['follower'] = follower
         
         return data
