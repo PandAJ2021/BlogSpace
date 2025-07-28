fc54727c9748aca595c20ca1baca3382e3d43527
AliJalili <pandaj200021@gmail.com>
2025-07-28 17:06:40 +0330

    Add routes for listing and retrieving premium posts

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import SimpleRouter
 from . import views
 
 router = SimpleRouter()
-router.register(r'posts', views.ReadOnlyPostView, basename='posts')
+router.register(r'posts', views.ReadOnlyPublicPostView, basename='posts')
 router.register(r'my-posts', views.UserPostView, basename='my_posts')
 router.register(r'my-comments', views.UserCommentView, basename='my-comments')
 
@@ -12,6 +12,8 @@ urlpatterns = [
     path('post/comments/', views.ListCommentsView.as_view(), name='post_comments'),
     path('post/like/<slug:post_slug>/', views.PostLikeView.as_view(), name='post_like'),
     path('post/comment/like/<int:comment_id>/', views.CommentLikeView.as_view(), name='comment_like'),
+    path('posts/premium/', views.ReadOnlyPremiumPostView.as_view({'get': 'list'}), name='premium-posts-list'),
+    path('posts/premium/<slug:slug>/', views.ReadOnlyPremiumPostView.as_view({'get': 'retrieve'}), name='premium-posts-detail'),
 
 ]
 urlpatterns += router.urls
