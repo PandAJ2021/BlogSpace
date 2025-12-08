0e10485a5371fae442364cdeb61764e2f0697ea3
AliJalili <pandaj200021@gmail.com>
2025-12-09 02:51:13 +0330

    add AdminUserSerializer tests for update logic, validation, and duplicate checks

--- accounts/tests/test_serializers.py
+++ accounts/tests/test_serializers.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from accounts.models import User
-from accounts.serializers import UserRegisterSerializer
+from accounts.serializers import UserRegisterSerializer, AdminUserSerializer
 
 
 class UserRegisterSerializerTests(TestCase):
@@ -64,3 +64,78 @@ class UserRegisterSerializerTests(TestCase):
         self.assertIn("email", serializer.errors)
 
 
+class AdminUserSerializerTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            phone="09987654321",
+            email="test@gmail.com",
+            username="testuser",
+            password="testpassword"
+        )
+
+    def test_update_without_password(self):
+        data = {
+            "phone":"09987654321",
+            "email":"newtest@gmail.com",
+            "username":"newtestuser",
+            "is_active":False,
+            "is_admin":True
+        }
+        serializer = AdminUserSerializer(data=data, instance=self.user, partial=True)
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        update_user = serializer.save()
+        self.assertEqual(update_user.phone, "09987654321")
+        self.assertEqual(update_user.email, "newtest@gmail.com")
+        self.assertEqual(update_user.username, "newtestuser")
+        self.assertTrue(update_user.is_admin)
+        self.assertFalse(update_user.is_active)
+        self.assertTrue(update_user.check_password("testpassword"))
+    
+    def test_update_with_password(self):
+        data = {
+            "password":"newtestpassword",
+        }
+        serializer = AdminUserSerializer(data=data, instance=self.user, partial=True)
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+        update_user = serializer.save()
+        self.assertFalse(update_user.check_password("testpassword"))
+
+    
+    def test_invalid_phone_number(self):
+        data = {
+            "phone":"08123456789",
+        }
+        serializer = AdminUserSerializer(data=data, instance=self.user, partial=True)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn("phone", serializer.errors)
+
+    def test_duplicate_username_not_allowed(self):
+        User.objects.create_user(
+            phone="09999999999",
+            email="othertest@gmail.com",
+            username="duplicatetestuser",
+            password="testpassword"
+        )
+
+        data = {
+            "username":"duplicatetestuser",
+        }
+        serializer = AdminUserSerializer(data=data, instance=self.user, partial=True)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn("username", serializer.errors)
+
+    def test_duplicate_email_not_allowed(self):
+        User.objects.create_user(
+            phone="09999999999",
+            email="duplicatetest@gmail.com",
+            username="othertetestuser",
+            password="testpassword"
+        )
+
+        data = {
+            "email":"duplicatetest@gmail.com",
+        }
+        serializer = AdminUserSerializer(data=data, instance=self.user, partial=True)
+        self.assertFalse(serializer.is_valid())
+        self.assertIn("email", serializer.errors)
